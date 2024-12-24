8709bbf8576b89cb041ac5a7e3c088755765fa8d
yinmiao <1879027167@qq.com>
2024-12-24 20:20:49 +0800

    update

---
+++ core/src/main/java/com/symaster/mrd/g2d/ActivityBlockSizeExtend.java
@@ -0,0 +1,11 @@
+package com.symaster.mrd.g2d;
+
+/**
+ * @author yinmiao
+ * @since 2024/12/24
+ */
+public interface ActivityBlockSizeExtend {
+
+    void update(Node node, float oldSize, float newSize);
+
+}
--- core/src/main/java/com/symaster/mrd/g2d/Scene.java
+++ core/src/main/java/com/symaster/mrd/g2d/Scene.java
@@ -1,6 +1,11 @@
 package com.symaster.mrd.g2d;
 
-import java.util.ArrayList;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 场景
@@ -8,43 +13,127 @@ import java.util.ArrayList;
  * @author yinmiao
  * @since 2024/12/22
  */
-public class Scene extends ArrayList<Node> {
+public class Scene {
 
-    public void render() {
-        for (Node node : this) {
-            node.update();
-        }
+    /**
+     * 区块大小
+     */
+    private final float blockSize;
+    /**
+     * 分区块Node表
+     */
+    private final Map<Block, Set<Node>> nodes;
 
-        // handleInput();
+    private final Map<Node, Block> activityBlock;
+
+    private final ActivityBlockSizeExtend activityBlockSizeExtend;
+
+    private final PositionUpdateExtend positionUpdateExtend;
+
+    public Scene() {
+        this(100f);
     }
 
-    // private void handleInput() {
-    //     if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-    //         cam.zoom += 0.02;
-    //     }
-    //     if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
-    //         cam.zoom -= 0.02;
-    //     }
-    //     if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
-    //         cam.translate(-3, 0, 0);
-    //     }
-    //     if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-    //         cam.translate(3, 0, 0);
-    //     }
-    //     if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-    //         cam.translate(0, -3, 0);
-    //     }
-    //     if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-    //         cam.translate(0, 3, 0);
-    //     }
-    //
-    //     cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100 / cam.viewportWidth);
-    //
-    //     float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
-    //     float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
-    //
-    //     cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
-    //     cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
-    // }
+    public Scene(float blockSize) {
+        this.blockSize = blockSize;
+        nodes = new HashMap<>();
+        this.activityBlock = new HashMap<>();
+        this.activityBlockSizeExtend = getActivityBlockSizeExtend();
+        this.positionUpdateExtend = getPositionUpdateExtend();
+    }
+
+    private PositionUpdateExtend getPositionUpdateExtend() {
+        return (node, oldX, oldY, newX, newY) -> {
+            Block oldIndex = getBlockIndex(oldX, oldY);
+            Block newIndex = getBlockIndex(newX, newY);
+            if (!oldIndex.equals(newIndex)) {
+                Set<Node> nodes1 = nodes.get(oldIndex);
+                if (nodes1 != null) {
+                    nodes1.remove(node);
+                }
+                nodes.computeIfAbsent(newIndex, k -> new HashSet<>()).add(node);
+            }
+        };
+    }
+
+    private ActivityBlockSizeExtend getActivityBlockSizeExtend() {
+        return (node, oldSize, newSize) -> {
+            if (newSize > 0) {
+                // 添加
+                Block block = activityBlock.get(node);
+                if (block == null) {
+                    activityBlock.put(node, getBlockIndex(node.getPositionX(), node.getPositionY()));
+                }
+            } else if (oldSize > 0) {
+                // 移除
+                activityBlock.remove(node);
+            }
+        };
+    }
+
+    public void add(Node node) {
+        Block blockIndex = getBlockIndex(node.getPositionX(), node.getPositionY());
+
+        nodes.computeIfAbsent(blockIndex, x -> new HashSet<>()).add(node);
+
+        node.setBse(activityBlockSizeExtend);
+
+        node.setPue(positionUpdateExtend);
+
+        if (node.getActivityBlockSize() > 0) {
+            activityBlock.put(node, blockIndex);
+        }
+    }
+
+    public void remove(Node node) {
+        Set<Node> nodes2 = nodes.get(getBlockIndex(node.getPositionX(), node.getPositionY()));
+        if (nodes2 == null) {
+            return;
+        }
+
+        node.setPue(null);
+        node.setBse(null);
+
+        nodes2.remove(node);
+    }
+
+    private Block getBlockIndex(float x, float y) {
+        return new Block((int) Math.floor(x / blockSize), (int) Math.floor(y / blockSize));
+    }
+
+    /**
+     * 逻辑处理
+     *
+     * @param delta Time in seconds since the last frame.
+     */
+    public void act(float delta) {
+        // for (Node node : this) {
+        //     node.update();
+        // }
+    }
+
+    public void render() {
+    }
+
+    private void drawCamera(CameraNode cameraNode) {
+        SpriteBatch spriteBatch = cameraNode.getSpriteBatch();
+
+        spriteBatch.setProjectionMatrix(cameraNode.getCamera().combined);
+        spriteBatch.begin();
+
+
+        // for (Node n2 : this) {
+        //     if (n2 instanceof SpriteNode) {
+        //         SpriteNode spriteNode = (SpriteNode) n2;
+        //         Sprite sprite = spriteNode.getSprite();
+        //
+        //
+        //     }
+        // }
+    }
+
+    public void resize(int width, int height) {
+
+    }
 
 }
